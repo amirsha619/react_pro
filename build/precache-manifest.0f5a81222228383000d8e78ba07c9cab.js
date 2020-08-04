14088df09b68347df851bdc6e145ad907fec22c7
Amirsha Dudgaon <adudgaon@wssllp.in>
2020-08-04 10:54:32 +0530

    first commit

---
+++ build/precache-manifest.0f5a81222228383000d8e78ba07c9cab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40f763ea5c64cc1bfcfae92e955c9ae3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5fa7659521ba6e5b2fb3",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "901583ee10f65197a134",
+    "url": "/static/js/2.c24a221a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c24a221a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fa7659521ba6e5b2fb3",
+    "url": "/static/js/main.4895a0da.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
